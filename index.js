faf934486f99f2aa74a5b2c8df8539e02ffdc684
cmarrero01 <cmarrero01@gmail.com>
2017-12-29 13:53:21 -0300

    Move up domain

--- index.js
+++ index.js
@@ -17,34 +17,12 @@ module.exports = {
             };
 
             data.article.domain = url.parse(articleUrl).host;
+            data.article.author = author.getAuthor(preppedHtml);
+            data.article.title = title.getTitle(preppedHtml);
+            data.article.content = content.getArticleContent(preppedHtml, data.host);
+            data.article.summary = summary.getSummary(preppedHtml, data.content);
+            callback(null, data);
 
-            if(data.article.domain === 'www.perfil.com'){
-                console.log("Get Perfil",data.article.domain);
-                author.getAuthorAsync(preppedHtml,function(author){
-                    data.article.author = author;
-
-                    content.getArticleContentForItemprop(preppedHtml,data.host,function(cleanContent){
-                        data.article.content = cleanContent;
-
-                        title.getTitleAsync(preppedHtml,function (title) {
-                            data.article.title = title;
-                            summary.getSummaryAsync(preppedHtml, data.content,function(summary){
-                                data.article.summary = summary;
-                                callback(null, data);
-                            });
-                        });
-                    });
-                });
-
-            }else{
-                console.log("Get Perfil",data.article.domain);
-
-                data.article.author = author.getAuthor(preppedHtml);
-                data.article.title = title.getTitle(preppedHtml);
-                data.article.content = content.getArticleContent(preppedHtml, data.host);
-                data.article.summary = summary.getSummary(preppedHtml, data.content);
-                callback(null, data);
-            }
         });
     }
 }
